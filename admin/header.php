2ad4eca7046f960c3a71fda83bda988a3a2e6044
Eddy Castro <edcast10@gmail.com>
2015-10-08 15:38:12 -0600

    Flex Pool Project v1

---
+++ admin/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Flex Pool</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="../css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container createProfilePage">
+      <div class="col-sm-offset-2 col-sm-8 innerContainer">
+        <!-- header -->
+        <header class="col-sm-12">
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="../images/flex-pool-logo.gif" /></a>
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                  <li>
+                    <a href="#"><img src="../images/admin/icon1.svg" class="hidden-xs" alt="Add Profile Icon"> Add Profile</a>
+                  </li>
+                  <li><a href="#"><img src="../images/admin/icon2.svg" class="hidden-xs" alt="Delete Profile Icon"> Delete Profile</a></li>
+                  <li><a href="#"><img src="../images/admin/icon3.svg" class="hidden-xs" alt="Availability Icon"> Availability</a></li>
+                </ul>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
+        </header><!-- End of header -->
+      </div>
+        <div class="col-sm-12 welcomeBar">
+          <div class="col-sm-offset-2 col-sm-8 adminBarContainer">Welcome, Adrian</div>
+        </div>
