ec3efe5472e11874719dbd41e5388abebafe7bc0
juanpamv3 <jpmv3@hotmail.com>
2015-10-09 15:32:21 -0600

    Actualización de la base de datos.
    Actualización de la base de datos con los campos de  agency, y date.

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 08, 2015 at 11:36 PM
+-- Generation Time: Oct 09, 2015 at 11:30 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -68,17 +68,25 @@ CREATE TABLE `profiles` (
   `category` int(11) NOT NULL,
   `role` text NOT NULL,
   `skills` text NOT NULL,
-  `availability` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+  `availability` text NOT NULL,
+  `agency` text NOT NULL,
+  `last_modified` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `profiles`
 --
 
-INSERT INTO `profiles` (`id`, `name`, `country`, `category`, `role`, `skills`, `availability`) VALUES
-(2, 'Daniel', 'CR', 3, 'designer', 'chao', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]'),
-(3, 'John Doe', 'CR', 3, 'Sr. Creative Designer', 'UI, Graphic / Web Design, Email Template Design, Branding, Image Editing, digital Assets, HTML/CSS, Ilustration.', '["available", "available", "available", "available", "available", "available", "available", "booked", "booked", "booked", "booked", "booked", "halftime", "halftime", "halftime", "halftime", "halftime", "available", "available", "available", "available", "available"]'),
-(12, 'carlos', 'CR', 1, 'designer', 'chao', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]');
+INSERT INTO `profiles` (`id`, `name`, `country`, `category`, `role`, `skills`, `availability`, `agency`, `last_modified`) VALUES
+(2, 'Daniel', 'CR', 3, 'designer', 'chao', '["available","booked","booked","booked","booked","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '2015-10-01'),
+(3, 'John Doe', 'CR', 3, 'Sr. Creative Designer', 'UI, Graphic / Web Design, Email Template Design, Branding, Image Editing, digital Assets, HTML/CSS, Ilustration.', '["available", "available", "available", "available", "available", "available", "available", "booked", "booked", "booked", "booked", "booked", "halftime", "halftime", "halftime", "halftime", "halftime", "available", "available", "available", "available", "available"]', '', '2015-10-02'),
+(12, 'javier', 'CR', 1, 'Web Designer', 'Web Designer and drupal', '["available","available","available","available","booked","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '2015-10-03'),
+(13, 'Juan Pablo MuÃ±oz Vargas', 'CR', 3, 'Front End Developer', 'Skills, Skills Skills Skills Skills Skills', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '0000-00-00'),
+(14, 'carlos', 'CR', 3, 'Web Designer', 'Web Designer', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '0000-00-00'),
+(15, 'carlos', 'CR', 3, 'Web Designer', 'Web Designer', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '0000-00-00'),
+(16, 'carlos', 'CR', 3, 'Web Designer', 'Web Designer', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '0000-00-00'),
+(17, 'javier', 'CR', 1, 'php developer1', 'php', '["available","available","available","booked","booked","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '', '2015-10-01'),
+(18, 'John disponibilidad probada', 'CR', 1, 'php developer', 'php', '["available","available","available","available","available","available","available","booked","booked","booked","booked","booked","halftime","halftime","halftime","halftime","halftime","available","available","available","available","available"]', '["","","","","","","","flex","flex","flex","flex","flex","","","","","","","","","",""]', '2015-10-09');
 
 --
 -- Indexes for dumped tables
@@ -121,7 +129,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- Constraints for dumped tables
 --
